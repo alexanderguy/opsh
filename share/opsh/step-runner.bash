7daf1c023276bc1c151abb0b82a8f6ed16c0fdbe
Alexander Guy <alexander.guy@pm.me>
2024-10-05 01:56:11 +0900

    Add support for running steps

---
+++ share/opsh/step-runner.bash
@@ -0,0 +1,19 @@
+# Run a series of functions, in order, starting with a prefix.
+steps::run() {
+    local prefix start name
+
+    prefix=$1 ; shift
+    start=""
+
+    if [[ $# -gt 0 ]]; then
+	start="${prefix}::$1" ; shift
+	log::warn "starting steps with $start..."
+    fi
+
+    while read -r name; do
+	if [[ $name > $start || $name = "$start" ]]; then
+	    log::info "running step $name..."
+	    $name
+	fi
+    done < <(declare -F | grep "$prefix::" | awk '{ print $3; }')
+}
