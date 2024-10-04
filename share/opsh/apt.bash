45018cc7300f6104c37edb7cf2b2dbc43b890076
Alexander Guy <alexander.guy@pm.me>
2024-10-05 02:01:06 +0900

    Add some apt assistance functions

---
+++ share/opsh/apt.bash
@@ -0,0 +1,16 @@
+apt::env () {
+    # Disable interactive frontends by default.
+    DEBIAN_FRONTEND=noninteractive
+    export DEBIAN_FRONTEND
+    
+    : "${SUDO:=sudo --preserve-env=DEBIAN_FRONTEND}"
+    : "${APT:=$SUDO apt-get -qy}"
+}
+
+apt::update () {
+    ( apt::env ; $APT update "$@" )
+}
+
+apt::install () {
+    ( apt::env ; $APT install "$@" )
+}
