9101110d92fc9dcf456d3ebc6776f722a6a09a13
Alexander Guy <alexander.guy@pm.me>
2024-12-05 00:27:27 -0500

    Add some more git helper functions

--- share/opsh/git.bash
+++ share/opsh/git.bash
@@ -1,3 +1,18 @@
 git::repo::version() {
     git describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD
 }
+
+git::repo::current-branch() {
+    git rev-parse --abbrev-ref HEAD
+}
+
+git::repo::is-clean() {
+    [[ $(git status --porcelain | wc -c) -eq 0 ]] || return 1
+}
+
+git::tag::exists() {
+    local tag=$1
+    shift
+
+    [[ $(git tag -l "$tag") == "$tag" ]] || return 1
+}
